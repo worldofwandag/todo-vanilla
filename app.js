fe558d4a19a3a1607901e8b77ec4f86d6a30249d
worldofwandag <jwandag@gmail.com>
2025-09-15 12:07:51 -0700

    practice todo

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
-const list = document.querySelector('.list');
-const input = document.querySelector('input');
+const input = document.querySelector('input')
+const list = document.querySelector('.list')
 
 let todoList = [];
 let todoInputValue = '';
 let counter = 0;
 
 function onInputChange(event) {
-  todoInputValue = event.target.value
+  todoInputValue = event.target.value;
+}
+
+function handleInputKeyUp(event) {
+  if(event.key === 'Enter'){
+    addTodo()
+  }
 }
 
 function addTodo() {
-  if (!todoInputValue) {
+  if(!todoInputValue) {
     return
   }
+
   todoList.push({
     id: counter++,
     task: todoInputValue,
   })
-  renderTodos();
-  input.value = '';
-  todoInputValue = '';
-}
-
-function handleInputKeyUp(event) {
-  if (event.key === "Enter") {
-    addTodo();
-  }
-}
-
-function deleteTodo(id) {
-  todoList = todoList.filter((todo) => todo.id !== id)
   renderTodos()
+  input.value = ''
+  todoInputValue = ''
 }
 
 function renderTodos() {
-  list.innerHTML = todoList.map((element) => 
+  list.innerHTML = todoList.map((element) =>
   `<li>
           ${element.task}
           <button class="todo__delete" onclick="deleteTodo(${element.id})">
             x
           </button>
         </li>
-  `).join('')
+  `
+  ).join('')
+}
+
+function deleteTodo(id) {
+  todoList = todoList.filter((todo) => todo.id !== id)
+  renderTodos()
 }
